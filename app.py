52f7ca49c5da098afbf47f3dddac18d20def8391
Axel Castellanos <acastellanosmorales@csumb.edu>
2025-03-06 10:55:02 -0800

    adding templates

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Flask heroku app. Hello"
+    return render_template('home.html')
+
+@app.route('/search_engine')
+def search():
+    return render_template('search_engine.html')
+
+@app.route('/repo_explorer')
+def github():
+    return render_template('repo_explorer.html')
+
+@app.route('/bookmark')
+def bookmark():
+    return render_template('bookmark.html')
+
+@app.route('/AI_chatot')
+def bot():
+    return render_template('AI_bot.html')
+
+@app.route('/contribute')
+def contribute():
+    return render_template('contribute.html')
+
+@app.route('/learning_material')
+def learning():
+    return render_template('learn_materials.html')
+
 
 if __name__ == '__main__':
     app.run()
